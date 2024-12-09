5f4a580cf6cc39bf65942bbb31479a67058a3bbd
dandorsan <dandorsan@alum.us.es>
2024-12-09 14:43:02 +0100

    Mejoras en el código

--- server/appnl.mjs
+++ server/appnl.mjs
@@ -45,29 +45,38 @@ class AppNodeLibrary {
     return sesion.userdata;
   }
 
-  async getAutorizacion(sesionid){
-    const sesion = login.getSesion(sesionid);
-    const userData = await this.obtenerDatosUsuario(sesionid)
-
-    if(!userData){
-      return {err:true, errmsg: 'Esta cuenta no existe'}
+  async getAutorizacion(sesionid) {
+    try {
+      const userData = await this.obtenerDatosUsuario(sesionid);
+  
+      if (!userData) {
+        return { err: true, errmsg: 'Esta cuenta no existe' };
+      }
+      if (userData.estado === 0) {
+        return { err: true, errmsg: 'Cuenta deshabilitada' };
+      }
+  
+      const conexion = await this.connectPostgreSQL();
+      const query = {
+        text: `SELECT f.nombre AS funcionalidad 
+        FROM Usuario u 
+        JOIN roles r ON u.username = r.username_user 
+        JOIN rol_funcionalidad rf ON r.permisos = rf.rol 
+        JOIN funcionalidades f ON rf.id_funcionalidad = f.id 
+        WHERE u.username = $1`,
+        values: [userData.username],
+      };
+  
+      const res = await conexion.query(query);
+      await conexion.end();
+      return res.rows;
+  
+    } catch (error) {
+      console.error('Error al obtener las autorizaciones:', error);
+      return { err: true, errmsg: 'Error al obtener las autorizaciones' };
     }
-    if (userData.estado === 0){
-      return {err:true, errmsg:'Cuenta deshabilitado'}
-    }
-    const conexion = await this.connectPostgreSQL();
-    const query = {
-      text: `SELECT f.nombre AS funcionalidad 
-      FROM Usuario u
-      JOIN roles r ON u.username = r.username_user
-      JOIN rol_funcionalidad rf ON r.permisos = rf.rol
-      JOIN funcionalidades f ON rf.id_funcionalidad = f.id
-      WHERE u.username = '${userData.username}'`,
-    };
-    const res = await conexion.query(query);
-    await conexion.end();
-    return res.rows
   }
+  
 }
 const appnl = new AppNodeLibrary();
 export default appnl;
