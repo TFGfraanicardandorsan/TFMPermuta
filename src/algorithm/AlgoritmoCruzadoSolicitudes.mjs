13dcbc328f42dc142c3ef3a3c31931adace51586
FranAnillo <=>
2025-02-19 21:20:40 +0100

    Algoritmo de cruzado de datos v2

--- src/algorithm/AlgoritmoCruzadoSolicitudes.mjs
+++ src/algorithm/AlgoritmoCruzadoSolicitudes.mjs
@@ -1,15 +1,28 @@
 class PermutaMatching {
-    constructor(estudiantes, solicitudes) {
-        this.estudiantes = new Set(estudiantes);
-        this.solicitudes = solicitudes;
+    constructor(estudiantes, permutas) {
+        this.estudiantes = estudiantes;
+        this.permutas = permutas;
         this.grafo = new Map();
     }
 
     construirGrafo() {
-        this.estudiantes.forEach(estudiante => this.grafo.set(estudiante, []));
-        this.solicitudes.forEach(([a, b]) => {
-            this.grafo.get(a).push(b);
-            this.grafo.get(b).push(a);
+        // Inicializar el grafo con los estudiantes
+        this.estudiantes.forEach(estudiante => {
+            this.grafo.set(estudiante.id, new Map());
+        });
+
+        // Crear aristas basadas en las solicitudes de permuta, considerando asignaturas
+        this.permutas.forEach(({ estudianteId, permutaA, asignatura }) => {
+            const estudianteActual = this.estudiantes.find(e => e.id === estudianteId);
+            const candidato = this.estudiantes.find(e => e.grupo === permutaA);
+
+            if (candidato) {
+                const conexiones = this.grafo.get(estudianteId);
+                if (!conexiones.has(candidato.id)) {
+                    conexiones.set(candidato.id, new Set());
+                }
+                conexiones.get(candidato.id).add(asignatura);
+            }
         });
     }
 
@@ -17,15 +30,22 @@ class PermutaMatching {
         const visitado = new Set();
         const emparejamiento = [];
 
-        for (let estudiante of this.estudiantes) {
+        for (let estudiante of this.grafo.keys()) {
             if (!visitado.has(estudiante)) {
-                for (let vecino of this.grafo.get(estudiante)) {
-                    if (!visitado.has(vecino)) {
-                        emparejamiento.push([estudiante, vecino]);
-                        visitado.add(estudiante);
-                        visitado.add(vecino);
-                        break;
+                let mejorVecino = null;
+                let maxAsignaturas = 0;
+
+                for (let [vecino, asignaturas] of this.grafo.get(estudiante)) {
+                    if (!visitado.has(vecino) && asignaturas.size > maxAsignaturas) {
+                        mejorVecino = vecino;
+                        maxAsignaturas = asignaturas.size;
                     }
+                }
+
+                if (mejorVecino) {
+                    emparejamiento.push([estudiante, mejorVecino, Array.from(this.grafo.get(estudiante).get(mejorVecino))]);
+                    visitado.add(estudiante);
+                    visitado.add(mejorVecino);
                 }
             }
         }
@@ -33,7 +53,8 @@ class PermutaMatching {
     }
 }
 
-const permuta = new PermutaMatching(estudiantes, solicitudes);
+
+const permuta = new PermutaMatching(estudiantes, permutas);
 permuta.construirGrafo();
 const permutasOptimas = permuta.emparejar();
 console.log("Permutas óptimas:", permutasOptimas);
