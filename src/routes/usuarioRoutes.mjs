50f4850e41ce2ccffb9a9ff56222c8a55a365edf
dandorsan <dandorsan@alum.us.es>
2025-03-05 23:22:33 +0100

    eliminación de comentarios

--- src/routes/usuarioRoutes.mjs
+++ src/routes/usuarioRoutes.mjs
@@ -3,7 +3,6 @@ import usuarioController from '../controllers/usuarioController.mjs';
 import { verificarRol } from '../middleware/rolMiddleware.mjs';
 const router = Router()
 router
-// .post('/obtenerDatosUsuario', usuarioController.obtenerDatosUsuario)
 .post('/obtenerDatosUsuario',verificarRol('administrador'), usuarioController.obtenerDatosUsuario)
 .post('/actualizarEstudiosUsuario',usuarioController.actualizarEstudiosUsuario)
 
