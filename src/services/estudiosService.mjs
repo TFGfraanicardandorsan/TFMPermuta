72e3f4e2206b7671aa7a1023232e98afbdd54f4c
FranAnillo <fraanicar@alum.us.es>
2025-03-06 21:45:52 +0100

    Obtener estudios

--- src/services/estudiosService.mjs
+++ src/services/estudiosService.mjs
@@ -4,12 +4,22 @@ class EstudiosService{
     async obtenerMiEstudioUsuario() {
         const conexion = await database.connectPostgreSQL();
         const query = {
-          text: `select e.nombre from estudios e where id = (select u.estudios_id_fk  from usuario u where u.nombre_usuario ='fraanicar');`,
+          text: `select e.nombre from estudios e where id = (select u.estudios_id_fk  from usuario u where u.nombre_usuario =$1);`,
+          values: [`${uvus}`],
         };
         const res = await conexion.query(query);
         await conexion.end();
         return res.rows;
-      }    
+      }   
+      async obtenerEstudios() {
+        const conexion = await database.connectPostgreSQL();
+        const query = {
+          text: `select e.nombre from estudios e;`,
+        };
+        const res = await conexion.query(query);
+        await conexion.end();
+        return res.rows;
+      }  
 }
 const estudiosService = new EstudiosService();
 export default estudiosService;
