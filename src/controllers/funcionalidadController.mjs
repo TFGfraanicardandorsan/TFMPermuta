195a38470179f829413a3284aa3b8e6e7cdab0d3
dandorsan <dandorsan@alum.us.es>
2025-03-10 21:22:47 +0100

    implementado el rol de administrador para funcionalidad

--- src/controllers/funcionalidadController.mjs
+++ src/controllers/funcionalidadController.mjs
@@ -2,9 +2,11 @@ import funcionalidadService from "../services/funcionalidadService.mjs";
 
 
 const insertarFuncionalidad = async (req,res) => {
-    try{
-        const insertarFuncionalidad = await funcionalidadService.insertarFuncionalidad(req.body.funcionalidad);
-        res.send({err:false, result:insertarFuncionalidad})
+    try {
+        if (!req.session.user) {
+            return res.status(401).json({ err: true, message: "No hay usuario en la sesión" });
+        }
+        res.send({err:false, result: await funcionalidadService.insertarFuncionalidad(req.body.funcionalidad)})
         } catch (err){
             console.log('api insertarFuncionalidad ha tenido una excepción')
             res.sendStatus(500)
--- src/routes/funcionalidadRoutes.mjs
+++ src/routes/funcionalidadRoutes.mjs
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import funcionalidadController from '../controllers/funcionalidadController.mjs';
+import { verificarRol } from '../middleware/rolMiddleware.mjs';
 
 const router = Router()
 router
-.post('/insertarFuncionalidad', funcionalidadController.insertarFuncionalidad)
+.post('/insertarFuncionalidad',verificarRol('administrador'), funcionalidadController.insertarFuncionalidad)
 
 export default router;
