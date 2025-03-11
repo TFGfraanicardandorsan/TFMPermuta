5026c97da401c2e2ab26c0e29ee01230a9e03a56
dandorsan <dandorsan@alum.us.es>
2025-03-11 22:22:43 +0100

    importación de la clase

--- src/controllers/usuarioGrupoController.mjs
+++ src/controllers/usuarioGrupoController.mjs
@@ -1,12 +1,11 @@
-import UsuarioGrupoService from "../services/usuarioGrupoService.mjs";
-
+import usuarioGrupoService from "../services/usuarioGrupoService.mjs";
 const insertarGrupoAsignatura = async (req, res) => {
     try {
         if (!req.session.user) {
             return res.status(401).json({ err: true, message: "No hay usuario en la sesión" });
         }
         const uvus = req.session.user.nombre_usuario;
-        res.send({ err: false, result: await UsuarioGrupoService.insertarGrupoAsignatura(uvus, req.body.grupo, req.body.asignatura)});
+        res.send({ err: false, result: await usuarioGrupoService.insertarGrupoAsignatura(uvus, req.body.grupo, req.body.asignatura)});
     } catch (err) {
         console.log('api insertarGrupoAsignatura ha tenido una excepción')
         res.sendStatus(500)
