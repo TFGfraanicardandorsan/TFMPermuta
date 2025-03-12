f7cc7ecd73152d029a76da94577f50eddd426554
dandorsan <dandorsan@alum.us.es>
2025-03-12 23:15:13 +0100

    almacenamiento

--- src/middleware/almacenamiento.mjs
+++ src/middleware/almacenamiento.mjs
@@ -1,12 +1,16 @@
 import multer from "multer";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
+import { fileURLToPath } from "url";
 
-const UPLOADS_FOLDER = process.env.UPLOADS_FOLDER 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const UPLOADS_FOLDER = process.env.UPLOADS_FOLDER;
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         console.log("Guardando en:", UPLOADS_FOLDER); 
-        cb(null, UPLOADS_FOLDER);
+        cb(null, path.join(__dirname, UPLOADS_FOLDER));
     },
     filename: (req, file, cb) => {
         const extension = path.extname(file.originalname);
