1d723888af7f3533341ec54c3e8bbd54d423ebd1
FranAnillo <fraanicar@alum.us.es>
2025-03-13 22:31:09 +0100

    arreglar insert

--- src/services/usuarioAsignaturaService.mjs
+++ src/services/usuarioAsignaturaService.mjs
@@ -16,7 +16,7 @@ async actualizarAsignaturasUsuario(uvus,asignatura) {
     try {
     const query = {
       text: `insert into usuario_asignatura values (
-              (select u.estudios_id_fk from usuario u where u.nombre_usuario =$1),  (select id from asignatura where codigo = $2))`,
+              (select u.id from usuario u where u.nombre_usuario =$1),  (select id from asignatura where codigo = $2))`,
       values: [`${uvus}`, `${asignatura}`],
     };
     await conexion.query(query);
