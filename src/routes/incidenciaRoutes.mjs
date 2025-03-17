9b60e6559d0a57eec624913a6a4db83678a664a8
dandorsan <dandorsan@alum.us.es>
2025-03-17 20:33:26 +0100

    cambiado post

--- src/routes/incidenciaRoutes.mjs
+++ src/routes/incidenciaRoutes.mjs
@@ -4,10 +4,10 @@ import { verificarRol } from '../middleware/rolMiddleware.mjs';
 
 const router = express.Router();
 
-router.get("/obtenerIncidencias", verificarRol('administrador'), incidenciaController.obtenerIncidencias);
-router.get("/obtenerIncidenciasAsignadasUsuario", verificarRol('estudiante'), incidenciaController.obtenerIncidenciasAsignadasUsuario);
-router.get("/obtenerIncidenciasAsignadas",verificarRol('administrador'), incidenciaController.obtenerIncidenciasAsignadas);
-router.get("/obtenerIncidenciasSinAsignar",verificarRol('administrador'),incidenciaController.obtenerIncidenciasSinAsignar);
+router.post("/obtenerIncidencias", verificarRol('administrador'), incidenciaController.obtenerIncidencias);
+router.post("/obtenerIncidenciasAsignadasUsuario", verificarRol('estudiante'), incidenciaController.obtenerIncidenciasAsignadasUsuario);
+router.post("/obtenerIncidenciasAsignadas",verificarRol('administrador'), incidenciaController.obtenerIncidenciasAsignadas);
+router.post("/obtenerIncidenciasSinAsignar",verificarRol('administrador'),incidenciaController.obtenerIncidenciasSinAsignar);
 router.post("/asignarmeIncidencia",verificarRol('administrador'), incidenciaController.asignarmeIncidencia);
 router.post("/solucionarIncidencia",verificarRol('administrador'), incidenciaController.solucionarIncidencia);
 router.post("/crearIncidencia",verificarRol('estudiante'), incidenciaController.crearIncidencia);
