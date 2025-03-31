a266a3b21deaab8536aa17f352e49fdb5e271eaf
FranAnillo <fraanicar@alum.us.es>
2025-03-31 22:26:35 +0200

    notificación fixed

--- src/services/notificacionService.mjs
+++ src/services/notificacionService.mjs
@@ -11,15 +11,15 @@ class NotificacionService{
         return res;
       }
 
-      async getNotificacionesUsuario(){
+      async getNotificacionesUsuario(uvus){
         const conexion = await database.connectPostgreSQL();
         const query = {
-          text: `select * from notificacion n where receptor = (select rol  from roles r where usuario_id_fk = (select id from usuario u where u.nombre_usuario =$1) ) or receptor = 'all'`,
+          text: `select contenido,fecha_creacion from notificacion n where receptor = (select rol  from roles r where usuario_id_fk = (select id from usuario u where u.nombre_usuario =$1) ) or receptor = 'all'`,
           values: [`${uvus}`],
         };
         const res = await conexion.query(query);
         await conexion.end();
-        return res;
+        return res.rows;
       }
 }
 const notificacionService = new NotificacionService();
