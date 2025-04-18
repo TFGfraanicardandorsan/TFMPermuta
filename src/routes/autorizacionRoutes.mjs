a67413cfaaac75fb539139ea6ad0243f931edbc1
dandorsan <dandorsan@alum.us.es>
2025-04-19 01:46:56 +0200

    logout

--- src/routes/autorizacionRoutes.mjs
+++ src/routes/autorizacionRoutes.mjs
@@ -8,31 +8,7 @@ router.get('/saml/login', (req, res, next) => {
     passport.authenticate('saml')(req, res, next);
 });
 router.get('/saml/session', verificarSesionUsuario)
-// router.get('/saml/logout', logout);
-router.get('/saml/logout', (req, res) => {
-    req.session.destroy(() => {
-      res.clearCookie('connect.sid');
-      // Se envía una página HTML intermedia que redirige inmediatamente
-      res.send(`
-        <!DOCTYPE html>
-        <html lang="es">
-        <head>
-          <meta charset="UTF-8">
-          <title>Cerrando sesión...</title>
-          <!-- Redirección automática inmediata con meta refresh -->
-          <meta http-equiv="refresh" content="0;url=https://permutas.eii.us.es/simplesaml/module.php/core/authenticate.php?as=default-sp&logout&ReturnTo=https://permutas.eii.us.es">
-        </head>
-        <body>
-          <p>Cerrando sesión, por favor espere...</p>
-          <script>
-            // Redirección de respaldo con JavaScript
-            window.location.href = "https://permutas.eii.us.es/simplesaml/module.php/core/authenticate.php?as=default-sp&logout&ReturnTo=https://permutas.eii.us.es";
-          </script>
-        </body>
-        </html>
-      `);
-    });
-  });
+router.get('/saml/logout', logout);
 export default router;
 
 
