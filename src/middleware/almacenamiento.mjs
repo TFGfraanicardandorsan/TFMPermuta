e99c6d0c6df6c586509fd49f12baaaae7357ee1e
dandorsan <dandorsan@alum.us.es>
2025-05-02 00:27:11 +0200

    permitir guardar archivos en dos carpetas diferentes

--- src/middleware/almacenamiento.mjs
+++ src/middleware/almacenamiento.mjs
@@ -3,13 +3,22 @@ import path from "path";
 import { v4 as uuidv4 } from "uuid";
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, process.env.UPLOADS_FOLDER);
-    },
-    filename: (req, file, cb) => {
-        const extension = path.extname(file.originalname);
-        cb(null, uuidv4() + extension);
+  destination: (req, file, cb) => {
+    const tipo = req.body.tipo;
+    let uploadPath;
+    if (tipo === "buzon") {
+      uploadPath = process.env.BUZON;
+    } else if (tipo === "archivador") {
+      uploadPath = process.env.ARCHIVADOR;
+    } else {
+      return cb(new Error("Tipo de carpeta no válido"));
     }
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const extension = path.extname(file.originalname);
+    cb(null, uuidv4() + extension);
+  },
 });
 
 const upload = multer({ storage });
