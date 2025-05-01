7a46c36b513cab5d8d7b1e38dbf9b7adf7dd5dc7
dandorsan <dandorsan@alum.us.es>
2025-05-01 13:59:59 +0200

    api crearListaPermutas

--- src/services/permutaService.mjs
+++ src/services/permutaService.mjs
@@ -1,15 +1,35 @@
 import database from "../config/database.mjs";
 
 class PermutaService{
-    async getPermutas(){
-        const conexion = await database.connectPostgreSQL();
-        const query = {
-          text: `select * from permuta`,
+  async crearListaPermutas(archivo,IdsPermuta) {
+    const conexion = await database.connectPostgreSQL();
+    try {
+      await conexion.query('BEGIN');
+      const queryPermutas = {
+        text: ` INSERT INTO permutas (estado, archivo ) VALUES ('FIRMADA',$1) 
+                RETURNING id`,
+        values: [archivo],
+      };
+      const resultado = await conexion.query(queryPermutas);
+      const permutasId = resultado.rows[0].id;
+      for (const id of IdsPermuta) {
+        const queryPermutas_permuta = {
+          text: ` INSERT INTO permutas_permuta (permuta_id_fk, permutas_id_fk) 
+                  VALUES ($1, $2)`,
+          values: [id,permutasId],
         };
-        const res = await conexion.query(query);
-        await conexion.end();
-        return res;
+        await conexion.query(queryPermutas_permuta);
       }
+      await conexion.query('COMMIT');
+      return "Se ha creado la lista de permutas correctamente";
+    } catch (error) {
+      await conexion.query('ROLLBACK');
+      console.error("Error al listar permutas:", error);
+      throw new Error("Error al listar permutas");
+    } finally {
+      await conexion.end();
+    }
+  }
 }
 const permutaService = new PermutaService();
 export default permutaService;
