3dd4ebc172d5a39b20329d4ab655f675cd99602f
FranAnillo <=>
2025-05-06 23:07:42 +0200

    Update listarPermutas to include FINALIZADA status in query filter

--- src/services/permutaService.mjs
+++ src/services/permutaService.mjs
@@ -273,7 +273,7 @@ class PermutaService {
           WHERE (
             p.usuario_id_1_fk = (SELECT id FROM usuario WHERE nombre_usuario = $1)
             OR p.usuario_id_2_fk = (SELECT id FROM usuario WHERE nombre_usuario = $1)
-          ) AND p.estado = 'VALIDADA' AND p.aceptada_1 = true
+          ) AND (p.estado = 'VALIDADA' OR p.estado ='FINALIZADA') AND p.aceptada_1 = true
             AND p.aceptada_2 = true
         `,
         values: [uvus],
