1275f66f93ae3faa46d3d6cfed580d02333481b5
dandorsan <dandorsan@alum.us.es>
2025-05-20 18:32:53 +0200

    VALIDADORES

--- src/controllers/uploadController.mjs
+++ src/controllers/uploadController.mjs
@@ -1,10 +1,13 @@
 import path from 'path';
+import GenericValidators from '../utils/genericValidators.mjs';
 
 const subirArchivo = (req, res) => {
     if (!req.file) {
         return res.status(400).send('No se ha subido ningún archivo');
     }
-
+    if (!GenericValidators.isString(req.file, 50)) {
+        return res.status(400).send("Nombre de archivo no válido (debe ser PDF o PNG)");
+    }
     return res.status(200).json({
         message: "Archivo subido correctamente",
         fileId: req.file.filename   
@@ -13,14 +16,19 @@ const subirArchivo = (req, res) => {
 
 const servirArchivo = (req, res) => {
     const { tipo, fileId } = req.params;
-    let baseDir;
-    if (tipo === "archivador") {
-        baseDir = process.env.ARCHIVADOR;
-    } else if (tipo === "buzon") {
-        baseDir = process.env.BUZON;
-    } else {
-        return res.status(400).send("Tipo de carpeta no válido");
+    if (tipo !== "archivador" && tipo !== "buzon") {
+        return res.status(400).send("Tipo de carpeta no válido (debe ser 'archivador' o 'buzon')");
     }
+    let baseDir = tipo === "archivador" ? process.env.ARCHIVADOR : process.env.BUZON;
+
+    if (!GenericValidators.isString(fileId, 50)) {
+        return res.status(400).send("Nombre de archivo no válido (debe ser PDF o PNG)");
+    }
+    const ext = path.extname(fileId).toLowerCase();
+    if (ext !== ".pdf" && ext !== ".png") {
+        return res.status(400).send("Solo se permiten archivos PDF o PNG");
+    }
+
     const filePath = path.join(baseDir, fileId);
     res.sendFile(filePath, (err) => {
         if (err) {
@@ -31,7 +39,7 @@ const servirArchivo = (req, res) => {
 };
 
 const obtenerPlantillaPermuta = (req, res) => {
-    const pdfPath = path.join(process.env.ARCHIVADOR, 'plantillaPermuta2425.pdf');
+    const pdfPath = path.join(process.env.PLANTILLAS, 'plantillaPermuta2425.pdf');
     res.sendFile(pdfPath, (err) => {
         if (err) {
             console.error('No se ha encontrado el archivo:', err);
